a3e45727404fab5f0055d0adfc06c0a5e3b2d006
Richard Lovell <richjavalabs@gmail.com>
2022-09-10 23:04:20 +1200

    Added vars

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@ module.exports = {
   publicRuntimeConfig: {
     API_URL: '',
     BACKEND_URL: '',
-    NAME: process.env.NAME,
-    BANK_ACCOUNT_NO: process.env.BANK_ACCOUNT_NO,
-    POSTAL_ADDRESS: process.env.POSTAL_ADDRESS,
+    NAME: 'Wanaka Concert Society',
+    BANK_ACCOUNT_NO: '03 1739 000 5648 01',
+    POSTAL_ADDRESS: 'P O Box 856, Wanaka 9343',
     EMAIL: 'info@wanakaconcertsociety.org.nz',
     NEWSLETTER_URL: 'https://wanakaconcertsociety.us18.list-manage.com/subscribe/post?u=9425e366bda64b3abe929b7f5&amp;id=edb480e86a'
   }
