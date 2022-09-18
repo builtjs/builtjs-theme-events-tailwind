90eeac7b5e384e7dda46fb83bf306c15f75caf8e
Richard Lovell <richjavalabs@gmail.com>
2022-09-18 14:36:15 +1200

    Updated image

--- components/templates/blocks/block-7/block-7.js
+++ components/templates/blocks/block-7/block-7.js
@@ -12,10 +12,8 @@ export default function Block7({ content }) {
                 <div className="w-full lg:w-3/5 md:mt-8 sm:mx-4 lg:ml-10">
                     <h4 className="mb-6">{attributes.subheading1}</h4>
                     <p className="text-primary-50 mb-12 lg:max-w-2xl md:pr-4 leading-7">{attributes.blurb1}</p>
-                    {/* {% call ctaBtnSimple.default({text: args.cta1, url: args.url1}) %}{% endcall %} */}
                     <h4 className="mb-6">{attributes.subheading2}</h4>
                     <p className="text-primary-50 mb-12 lg:max-w-2xl md:pr-4 leading-7">{attributes.blurb2}</p>
-                    {/* {% call ctaBtnSimple.default({text: args.cta2, url: args.url2}) %}{% endcall %} */}
                 </div>
             </div>
         </div>
