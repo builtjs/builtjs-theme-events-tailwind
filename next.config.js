4d9ba7ac4bd7e012cf9972b9dfcc4fe681f16552
Richard Lovell <richjavalabs@gmail.com>
2023-01-05 13:24:59 +1300

    updated to ts

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
-module.exports = {
-  publicRuntimeConfig: {
-    API_URL: '',
-    BACKEND_URL: '',
-    NAME: 'Events Tailwind',
-    BANK_ACCOUNT_NO: 'XX XXXX XXX XXXX XX',
-    POSTAL_ADDRESS: 'P O Box 123, Example 1234',
-    EMAIL: 'info@example.com',
-    NEWSLETTER_URL: ''
-  }
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
